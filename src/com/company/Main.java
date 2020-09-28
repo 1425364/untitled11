24906a664b244575d225f5e5360df5a73b5eb779
1425364 <you@example.com>
2020-09-28 20:55:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[]a; //.null
+        //a[0] = 1;
+        a = new int[5];
+        for (int i=0; i<a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        for(int p: a) System.out.print(p + " ");
+        int[] x = {5,6,7,-100};
+        
+
+
+    }
+}
